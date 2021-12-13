d609ef751d537838054dd2715de906bf69ff13b3
lukew3 <lukew25073@gmail.com>
2021-12-12 19:35:23 -0500

    made into single-page app

---
+++ src/defaultGame.js
@@ -0,0 +1,71 @@
+const { Game } = require("./Game.js");
+
+const boardSize = 15; // amount of cells in a row or column
+const totalRounds = 100; // number of rounds to render
+const roundTime = 1000; // Time to pause for after each round
+
+const boardObj = document.getElementById('gameBoard');
+const roundCtr = document.getElementById('roundCounter');
+const scoreP1 = document.getElementById('p1Score');
+const scoreP2 = document.getElementById('p2Score');
+const playerSwitch = document.getElementById("switch");
+let colorDead = "#EDEDED";
+let colorP1 = "blue";
+let colorP2 = "red";
+
+let gameObj = new Game({
+        boardObj, 
+        boardSize,  
+        totalRounds,
+        roundTime,
+        roundCtr,  
+        "colors": [
+                colorDead,
+                colorP1,
+                colorP2
+        ],
+        "scores": [
+                scoreP1,
+                scoreP2
+        ]
+});
+
+// Toggle cell
+document.addEventListener('click', (e) => {
+        let element = e.target;
+        let playerId;
+        if (playerSwitch == undefined)
+                playerId = 1;
+        else
+                playerId = (playerSwitch.checked) ? 2 : 1;
+        if (element.className === "cell") {
+                gameObj.toggleCell(element, playerId);
+        };
+});
+
+document.getElementById('submitMoveButton').addEventListener('click', (e) => {
+        playerSwitch.checked = !playerSwitch.checked;
+        gameObj.runRound();
+});
+
+
+// 2pPlayground Buttons
+document.getElementById('startStopButton').addEventListener('click', (e) => {
+        if (gameObj.isRunning()) {
+                gameObj.stopGame();
+                document.getElementById('startStopButton').innerHTML = "Start";
+        } else {
+                gameObj.runGame();
+                document.getElementById('startStopButton').innerHTML = "Stop";
+        }
+});
+
+document.getElementById('nextButton').addEventListener('click', (e) => {
+        gameObj.runRound();
+});
+
+document.getElementById('resetButton').addEventListener('click', (e) => {
+        gameObj.resetBoard();
+        document.getElementById('startStopButton').innerHTML = "Start";
+});
+
---
+++ src/router.js
@@ -0,0 +1,29 @@
+require("./defaultGame.js");
+
+const defaultPage = document.getElementById('defaultGamePage');
+const rulesPage = document.getElementById('rulesPage');
+const newGamePage = document.getElementById('newGamePage');
+
+const setPage = (pageId) => {
+        defaultPage.style = 'display: none;';
+        rulesPage.style = 'display: none;';
+        newGamePage.style = 'display: none;';
+        document.getElementById(pageId).style = 'display: block;';
+}
+
+if (window.location.pathname === '/' || window.location.pathname === '/index.html') {
+        defaultPage.style = 'display: block;';
+}
+
+document.getElementById('navTitle').addEventListener('click', (e) => {
+        setPage('defaultGamePage');
+});
+
+document.getElementById('navRules').addEventListener('click', (e) => {
+        setPage('rulesPage');
+});
+
+document.getElementById('navPlay').addEventListener('click', (e) => {
+        setPage('newGamePage');
+});
+
