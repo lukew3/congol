5ac788593643d00a16523737acf3d1040b4e4e40
Luke Weiler <lukew25073@gmail.com>
2021-12-24 11:05:52 -0500

    moved config out of main, removed games array

--- app.js
+++ app.js
@@ -6,17 +6,18 @@ const socketio = require('socket.io');
 const { connectDB, mongoDB } = require("./mongodb");
 const { nanoid } = require('nanoid');
 
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+dotenv.config();
+const port = process.env.port || 3000;
+const indexPg = path.join(__dirname, './dist/index.html');
+
+app.use(express.static('dist'));
+
 async function main() {
   await connectDB();
-  const app = express();
-  const server = http.createServer(app);
-  const io = socketio(server);
-
-  dotenv.config();
-  const port = process.env.port || 3000;
-  const indexPg = path.join(__dirname, './dist/index.html');
-
-  app.use(express.static('dist'));
 
   // Gameplay
   const boardSize = 15;
@@ -30,7 +31,6 @@ async function main() {
     }
     return myarr;
   }
-  let games = [];
   const sendGameUpdate = async (socket, roomId) => {
   	// send update to move sender
     let gameData = await mongoDB().collection('games').findOne({'shortId': roomId});
